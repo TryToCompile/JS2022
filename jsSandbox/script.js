f55c8aa0866045993d5558d8ca2d2708e13a3939
TryToCompile <adam1999@tut.by>
2022-07-15 17:10:20 +0300

    feat: add some feats

--- jsSandbox/script.js
+++ jsSandbox/script.js
@@ -1,11 +1,17 @@
 
 
 const distinctKataBtn = document.querySelector('.distinct-kata-btn'); 
-
+const arraySumBtn = document.querySelector('.arraySum-kata-btn');
 
 distinctKataBtn.addEventListener('click', () => {
     let distinctKataPrompt = prompt('Enter a list of numbers with no separation:','123123123543241');
     alert('Heey, this is your sorted array! -> ['+distinct(distinctKataPrompt.split('').sort())+']');
+});
+
+arraySumBtn.addEventListener('click', () => {
+    let arraySumPrompt = prompt('Enter a list of numbers/strings/chars with no separation:','123123123543241');
+    arraySumPrompt = arraySumPrompt.split('').map(Number);
+    alert('Heey, this is your sum! -> '+arraySumRecursion(arraySumPrompt));
 });
 
 // solved Kata (distinct), 7kyu
@@ -18,7 +24,7 @@ function distinct(a) {
     });
     return arr;
   }
-  console.log(distinct([1,1,2]));  // [1,2]
+//  console.log(distinct([1,1,2]));  // [1,2]
 // end of Kata (distinct), 7kyu 
 
 
@@ -28,7 +34,7 @@ function arraySum(arr) {
     return arr.flat().reduce((a,b) => a+b);
 }
 // TODO: Check if reduce is slower than typical for loop 
-  console.log(arraySum([1, 2, [3, 4], 5])); // 15
+  //console.log(arraySum([1, 2, [3, 4], 5])); // 15
 // Kata (array sum), 7kyu (recursion)
 function arraySumRecursion(arr){
     if(arr.length === 0) return 0;
@@ -36,6 +42,6 @@ function arraySumRecursion(arr){
     if(typeof arr[0] === 'object') return arraySumRecursion(arr[0]) + arraySumRecursion(arr.slice(1));
     if(typeof arr[0] !== 'number' || typeof arr[0] !=='object') return arraySumRecursion(arr.slice(1));
 }
-// TODO:  Refactor 
-console.log(arraySumRecursion([1, 2, [1, 2],[3,[1, 2, 3, 5, 6, 7], 4],'str', 5])); // 
+// TODO:  Refactor, dont works with strings like 1234asd
+//console.log(arraySumRecursion([1, 2, [1, 2],[3,[1, 2, 3, 5, 6, 7], 4],'str', 5])); // 
 // end of Kata (array sum), 7kyu
